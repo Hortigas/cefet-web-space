db90ff051688591008d832dea5e0f8a89a1b7fcf
Hortigas <xxvsvitor98xx@gmail.com>
2021-11-28 23:37:48 -0300

    exercicio3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,10 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+for (let i of document.querySelectorAll('.botao-expandir-retrair')) {
+    i.addEventListener('click', (evt) => {
+        const button = evt.currentTarget;
+        button.innerHTML === '+' ? (button.innerHTML = '-') : (button.innerHTML = '+');
+        button.parentNode.classList.toggle('expandido');
+    });
+}
