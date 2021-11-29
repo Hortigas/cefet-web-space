66cc751e1fe870c452abf080c19170f9070c65e3
Hortigas <xxvsvitor98xx@gmail.com>
2021-11-28 23:06:14 -0300

    exercicio1 e exercicio2

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,8 @@
-// Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+document.querySelector('#calcular').addEventListener('click', () => {
+    let constante = document.querySelector('#constante').value;
+    let massa1 = document.querySelector('#massa1').value;
+    let massa2 = document.querySelector('#massa2').value;
+    let distancia = document.querySelector('#distancia').value;
+
+    document.querySelector('#resultado').value = ((constante * massa1 * massa2) / Math.pow(distancia, 2)).toExponential();
+});
